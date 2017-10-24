191c44ee24e02c5a1ea5096bce820fde42a14770
lmplaczkiewicz <Frederick.Placzkiewicz@LibertyMutual.com>
2017-10-24 09:51:06 -0400

    add change unit to object

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022155302) do
+ActiveRecord::Schema.define(version: 20171024134622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20171022155302) do
   create_table "locations", force: :cascade do |t|
     t.string "address"
     t.string "business"
-    t.integer "units"
+    t.integer "objects"
     t.string "company"
     t.integer "compliance"
     t.date "inspection"
