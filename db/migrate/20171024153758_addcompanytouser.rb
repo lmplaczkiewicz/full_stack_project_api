ee3eec209f95e15178f6f9901787c87f3ce6781b
lmplaczkiewicz <Frederick.Placzkiewicz@LibertyMutual.com>
2017-10-24 11:40:25 -0400

    add foreign key

---
+++ db/migrate/20171024153758_addcompanytouser.rb
@@ -0,0 +1,5 @@
+class Addcompanytouser < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :companies, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024141331) do
+ActiveRecord::Schema.define(version: 20171024153758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20171024141331) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
   create_table "examples", force: :cascade do |t|
@@ -55,6 +57,7 @@ ActiveRecord::Schema.define(version: 20171024141331) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "companies", "users"
   add_foreign_key "examples", "users"
   add_foreign_key "locations", "users"
 end
