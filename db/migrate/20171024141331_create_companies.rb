9c39c443226e11c5d578bd39b10c6fd038641cc4
lmplaczkiewicz <Frederick.Placzkiewicz@LibertyMutual.com>
2017-10-24 10:18:52 -0400

    add company table

---
+++ db/migrate/20171024141331_create_companies.rb
@@ -0,0 +1,12 @@
+class CreateCompanies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :manager
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024135159) do
+ActiveRecord::Schema.define(version: 20171024141331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.string "phone"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "examples", force: :cascade do |t|
     t.text "text", null: false
